6cf5c3aa45c90e9b17a327861899d813c86084a4
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-14 22:32:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# Warm up
+# numbers = [1, 2, 3, 4, 5]
+# sum = 0
+# # get the sum of all numbers
+# for numbers in range(1, 6):
+#   sum = numbers + numbers 
+#   print(sum)
+
+# for i in range(1, 6):
+#   for j in range(0, i):
+#     print(j + 1, end=" ")
+#   print()
+
+# Warm Up 2
+# number = input("Give me a numbe, positive or negative: ")
+# number = int(number)
+# if number > 0:
+#   print("the number is greater than zero, therefore being positive")
+# elif number < 0:
+#   print("the number is less than zero, therefore being negative")
+# else:
+#   print("your number is 0")
+
+
+# def HelloWorld():
+#   print("hello world")
+
+# HelloWorld()
+
+# def greeting(name):
+#   print("hello", name)
+
+# myname = input("What is your name? ")
+# greeting(myname)
+
+# def addnumber(inputnumber, inputnumber2):
+#   output = int(inputnumber) + int(inputnumber2)
+#   return output
+
+# inputnumber = input("Enter first number: ")
+# inputnumber2 = input("Enter second number: ")
+
+# print(addnumber(inputnumber, inputnumber2))
+
+# def mulnumber(num1, num2): 
+#   output = int(num1) * int(num2)
+#   return output
+
+# num1 = input("put a number: ")
+# num2 = input("put another number: ")
+
+# print(mulnumber(num1, num2))
+
+import turtle 
+p = turtle.Turtle()
+p.speed(1)
+p.forward(50)
+p.left(120)
+p.forward(50)
+p.left(120)
+p.forward(50)
+
+ 
